0a8d0818a9d38a318bdfdfd0b32fb930759eaede
root <root@DeviceDriverDevMachine.us4okr32kuwuvev1rjqho0245h.gx.internal.cloudapp.net>
2021-02-27 04:26:13 +0000

    [Update][working] working commit for char device - able to load the driver

---
+++ chardriver/.built-in.a.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vmohanav/workspace/modules/chardriver/built-in.a := rm -f /home/vmohanav/workspace/modules/chardriver/built-in.a; ar cDPrST /home/vmohanav/workspace/modules/chardriver/built-in.a 
--- chardriver/chardevdriver.c
+++ chardriver/chardevdriver.c
@@ -13,7 +13,7 @@ struct fake_device{
 
 
 //(2) To register our fake device we need a cdev object and some other varialbes
-sturct cdev *mcdev;  //my char device
+struct cdev *mcdev;  //my char device
 int major_number;    //will store our device major number - extracted from dev_t using macro - mknod /director/file c major minor
 int ret;             //will be used to hold return values of functions; this is because the kernel stack is very small, 
                      //so declaring variables all over the pass in our module functions eats up the stack very fast 
@@ -21,14 +21,22 @@ dev_t dev_num;       //will hold the major number that kernel gives us,
                      //name--> appears in /proc/device
 #define DEVICE_NAME "MYTESTDEVICE"
 
+
+//file operation declaration
+static int device_open(struct inode *, struct file *);
+static int device_close(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+
 //(6) Tell the kernel which functions to call when user operates on our device file
-sturct file_operations fops = {
+const struct file_operations fops = {
 	.owner = THIS_MODULE, //prevent unloading of this module when operation in use
 	.open = device_open, //points to the method to call when opening a device
 	.release = device_close, //points to the method to call when closing a device
 	.write = device_write, //points to the method to call when writing to the device
 	.read = device_read //points to the method to call when reading a device
-}
+};
 
 
 //(7)called on device_file open
@@ -39,20 +47,20 @@ int device_open(struct inode *inode, struct file *filp){
 
 	//only allow one process to open this device by using semaphore as mutual exclusive lock - mutex
 	if(down_interruptible(&virtual_device.sem) != 0){
-		printk(KERN_ALERT "MYTESTDEVICE: could not lock device during open");
+		printk(KERN_ALERT "MYTESTDEVICE: could not lock device during open\n");
 		return -1;
 	}
 
-	printk(KERN_INFO "MYTESTDEVICE: opened device");
+	printk(KERN_INFO "MYTESTDEVICE: opened device\n");
 	return 0;
 }
 
 //(8) called when user wants to get information from the device
-ssize_t device_read(struct file* filp, char* bufStoreData, size_t bufCount, loff t* curOffset){
+ssize_t device_read(struct file* filp, char* bufStoreData, size_t bufCount, loff_t* curOffset){
 	//take data from kernel space(device) to user space(process)
 	//copy_to_user(desitnation, source, sizeToTransfer)
-	printk(KERN_INFO "MYTESTDEVICE: Reading from device");
-	ret = copy_to_user(bufStoreData, virtual_device.data, bufCount);
+	printk(KERN_INFO "MYTESTDEVICE: Reading from device\n");
+	ret = raw_copy_to_user(bufStoreData, virtual_device.data, bufCount);
 	return ret;
 }
 
@@ -61,8 +69,8 @@ ssize_t device_write(struct file* filp, const char* bufSourceData, size_t bufCou
 	//send data from user to kernel
 	//copy_from_user (dest, source, count)
 	
-	printk(KERN_INFO "MYTESTDEVICE: writing to device");
-	ret = copy_from_user(virtual_device.data, bufSourceData, bufCount);
+	printk(KERN_INFO "MYTESTDEVICE: writing to device\n");
+	ret = raw_copy_from_user(virtual_device.data, bufSourceData, bufCount);
 	return ret;
 }
 
@@ -71,7 +79,8 @@ int device_close(struct inode *inode, struct file *filp){
 	//by calling up, which is opposite of down for semaphore, we release the mutex that we obtained at device open
 	//this has the effect of allowing other process to use the device now
 	up(&virtual_device.sem);
-	printk(KERN_INFO "MYTESTDEVICE: closed device");
+	printk(KERN_INFO "MYTESTDEVICE: closed device\n");
+	return 0;
 }
 
 
@@ -85,22 +94,22 @@ static int driver_entry(void){
 	ret = alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME);
 	
 	if(ret < 0){
-		printk(KERN_ALERT "MYTESTDEVICE: failed to allocate a major number");
+		printk(KERN_ALERT "MYTESTDEVICE: failed to allocate a major number\n");
 		return ret;
 	}
 	major_number = MAJOR(dev_num); //extracts the major number and store in our variable(MACRO)
-	printk(KERN_INFO "MYTESTDEVICE: major number is %d", major_number);
-	printk(KERN_INFO "\tuse \"mknod /dev/%s c %d 0\" for device file",DEVICE_NAME,major_number);
+	printk(KERN_INFO "MYTESTDEVICE: major number is %d\n", major_number);
+	printk(KERN_INFO "\tuse \"mknod /dev/%s c %d 0\" for device file\n",DEVICE_NAME,major_number);
 
 	//step(2)
-	mc_dev = cdev_alloc(); //create our cdev structure, initialized our cdev
-	mc_dev->ops = fops; //struct file_operations
-	mc_dev->owner = THIS_MODULE;
+	mcdev = cdev_alloc(); //create our cdev structure, initialized our cdev
+	mcdev->ops = &fops; //struct file_operations
+	mcdev->owner = THIS_MODULE;
 	//now that we created cdev, we have to add it to the kernel
 	//int cdev_add(struct cdev* dev, dev_t num, unsigned int count)
 	ret = cdev_add(mcdev, dev_num, 1);
 	if(ret < 0){
-		printk(KERN_ALERT "MYTESTDEVICE: unable to add cdev to kernel");
+		printk(KERN_ALERT "MYTESTDEVICE: unable to add cdev to kernel\n");
 		return ret;
 	}
 	//step (4) Initialize our semaphore
@@ -115,7 +124,7 @@ static void driver_exit(void){
 
 	//(b)
 	unregister_chrdev_region(dev_num,1);
-	printk(KERN_ALERT "MYTESTDEVICE: unloaded module");
+	printk(KERN_ALERT "MYTESTDEVICE: unloaded module\n");
 }
 
 //Inform the kernel where to start and stop with our module/driver
